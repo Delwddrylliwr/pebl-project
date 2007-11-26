cbcc09747ed7923dae42a1bafde9b6a22ed1dedb
abhikshah <abhikshah@3447e8dd-9f38-0410-a211-e3037a7e05e3>
2007-11-26 22:04:13 +0000

    for active-learning

--- src/pebl/posterior.py
+++ src/pebl/posterior.py
@@ -1,18 +1,26 @@
 import math
 from copy import deepcopy
+from itertools import izip
+
 import numpy as N
 from pebl import network
 from pebl.util import *
 import pydot
 
 class Posterior():
-    def __init__(self, nodes, adjacency_matrices, scores):
+    def __init__(self, nodes, adjacency_matrices=[], scores=[], sorted_scored_networks=[]):
         self.nodes = nodes
-        adjacency_matrices_and_scores = sorted(zip(adjacency_matrices, scores), cmp=lambda x,y:cmp(x[1],y[1]), reverse=True)
-        adjacency_matrices, scores = unzip(adjacency_matrices_and_scores)
 
-        self.adjacency_matrices = N.array(adjacency_matrices)
-        self.scores = N.array(scores)
+        if len(adjacency_matrices) and scores:
+            adjacency_matrices_and_scores = sorted(izip(adjacency_matrices, scores), cmp=lambda x,y:cmp(x[1],y[1]), reverse=True)
+            adjacency_matrices, scores = unzip(adjacency_matrices_and_scores)
+
+            self.adjacency_matrices = N.array(adjacency_matrices)
+            self.scores = N.array(scores)
+
+        elif sorted_scored_networks:
+            self.adjacency_matrices = N.array([n.edgelist.adjacency_matrix for n in sorted_scored_networks])
+            self.scores = N.array([n.score for n in sorted_scored_networks])
 
     def _consensus_matrix(self):
         norm_scores = normalize(N.exp(rescale_logvalues(self.scores)))
@@ -53,6 +61,4 @@ class Posterior():
         # entropy = -exp(scores)*scores
         lscores = rescale_logvalues(self.scores)
         return -N.sum(N.exp(lscores)*lscores)
-
- 
 
--- src/pebl/result.py
+++ src/pebl/result.py
@@ -1,60 +1,63 @@
 from posterior import Posterior
 import time
 import socket
+from bisect import insort
+from heapq import heappush, heapreplace
 from util import flatten
 from copy import deepcopy
 
 
-class LearnerRun:
-    def __init__(self, learner):
-        self.learner_class = learner.__class__.__name__
-        self.starttime = time.time()
-        self.stoptime = None
-        self.hostname = socket.gethostname()
+class _ScoredNetworkList(object):
+    def __init__(self, size):
+        self.size = size
+        self.list = []
 
-        self.edgelists = []
-        self.scores = []
+    def push(self, scorednet):
+        l = self.list
+        
+        if len(l) < self.size:
+            if scorednet not in l:
+                insort(l, deepcopy(scorednet))
+        elif scorednet.score > l[0].score and scorednet not in l:
+            l.remove(l[0])
+            insort(l, deepcopy(scorednet))
 
-    def stop(self):
-        self.stoptime = time.time()
+    def __len__(self):
+        return len(self.list)
 
+    def __iter__(self):
+        return self.list.__iter__()
+
+
+class _ScoredNetwork(object):
+    def __init__(self, edgelist, score):
+        self.edgelist = edgelist
+        self.score = score
+
+    def __cmp__(self, other):
+        return cmp(self.score, other.score)
+
+    def __eq__(self, other):
+        return self.score == other.score and self.edgelist == other.edgelist
 
 class LearnerResult:
+    networks_to_save = 1000
+
     def __init__(self, learner_):
         self.data = learner_.data
         self.nodes = learner_.network.nodes
-        self.runs = []
-        self.current_run = None
+        self.networks = _ScoredNetworkList(self.networks_to_save)
 
     def start_run(self, learner):
-        # if learner didn't stop last run (could happen if: threw uncaught exception during run)
-        if self.current_run:
-            self.stop_run()
-
-        self.runs.append(LearnerRun(learner))
-        self.current_run = self.runs[-1]
+        pass
 
     def stop_run(self):
-        self.current_run.stop()
-        self.current_run = None
-    
-    def add_network(self, network, score):
-        self.current_run.edgelists.append(deepcopy(network.edges))
-        self.current_run.scores.append(score)
-    
-    def merge(self, other):
-        self.runs.extend(other.runs)
+        pass 
+
+    def add_network(self, net, score):
+        self.networks.push(_ScoredNetwork(net.edges, score))
 
     @property
     def posterior(self):
-        unique_edgelists = []
-        unique_scores = []
-
-        for run in self.runs:
-            for edgelist, score in zip(run.edgelists, run.scores):
-                if (score not in unique_scores) or (edgelist not in unique_edgelists):
-                    unique_scores.append(score)
-                    unique_edgelists.append(edgelist)
-
-        return Posterior(self.nodes, [el.adjacency_matrix for el in unique_edgelists], unique_scores)
+        return Posterior(self.nodes, sorted_scored_networks=self.networks)
 
